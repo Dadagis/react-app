d961c39bb1ac0270d0d64cbdb1d1be61db2e4980
David <davidobob@laposte.net>
2020-08-04 10:20:09 +0200

    added Joi for validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi, { schema } from "joi-browser";
 import Input from "./common/input";
 
 export default class LoginForm extends Component {
@@ -7,18 +8,35 @@ export default class LoginForm extends Component {
     errors: {},
   };
 
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const result = Joi.validate(this.state.account, this.schema, options);
+
+    if (!result.error) {
+      return null;
+    }
+
     const errors = {};
+    for (let item of result.error.details) errors[item.path[0]] = item.message;
+    return errors;
 
-    const { account } = this.state;
-    if (account.username.trim() === "") {
-      errors.username = "Username is required";
-    }
-    if (account.password.trim() === "") {
-      errors.password = "Password is required";
-    }
+    // Old code without Joi-browser
+    // const errors = {};
 
-    return Object.keys(errors).length === 0 ? null : errors;
+    // const { account } = this.state;
+    // if (account.username.trim() === "") {
+    //   errors.username = "Username is required";
+    // }
+    // if (account.password.trim() === "") {
+    //   errors.password = "Password is required";
+    // }
+
+    // return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (e) => {
@@ -30,17 +48,23 @@ export default class LoginForm extends Component {
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") {
-        return "Username is required";
-      }
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
 
-    if (name === "password") {
-      if (value.trim() === "") {
-        return "Password is required";
-      }
-    }
+    // Old code without Joi-browser
+    // if (name === "username") {
+    //   if (value.trim() === "") {
+    //     return "Username is required";
+    //   }
+    // }
+
+    // if (name === "password") {
+    //   if (value.trim() === "") {
+    //     return "Password is required";
+    //   }
+    // }
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -78,10 +102,11 @@ export default class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
   }
 }
-// 13 Joi
